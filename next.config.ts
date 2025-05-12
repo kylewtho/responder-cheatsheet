e128508b909b9c58963619664cefad02fd9d4525
Kyle <kyle@kyleho.net>
2025-05-12 17:59:06 +1000

    Deployed to Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
+  assetPrefix: '/responder',
 };
 
 export default nextConfig;
