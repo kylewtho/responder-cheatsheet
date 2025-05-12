d70a7036261f42f3c5a33a77e8cc938f27d55f94
Kyle <kyle@kyleho.net>
2025-05-12 17:48:09 +1000

    Added MARCHE page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/responder',
+  trailingSlash: true,
 };
 
 export default nextConfig;
